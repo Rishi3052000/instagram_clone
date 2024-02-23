e02ae5e3a5263e5247ca7d23e1eed526363b963b
Rishi Kant <rishi3052000@gmail.com>
2024-02-24 01:04:21 +0530

    Initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 5, maximum: 100}
+    validates :description, presence: true, length: {minimum: 5, maximum: 1000}
+    validates :keywords, presence: true, length: {minimum: 5, maximum: 100}
+
+    has_many_attached :images
+
+    belongs_to :user
+end
